a86fa30b5d65d359c096f3b84855fb4af869bf23
Tsmorz <tonysmoragiewicz@gmail.com>
2024-07-27 23:07:53 -0400

    receive motor commands from phone over BLE

--- ESP32-S3/bluetooth.py
+++ ESP32-S3/bluetooth.py
@@ -4,23 +4,23 @@
 # CircuitPython NeoPixel Color Picker Example
 
 import board
-import neopixel
+import busio
 
 from adafruit_bluefruit_connect.packet import Packet
-from adafruit_bluefruit_connect.color_packet import ColorPacket
 from adafruit_bluefruit_connect.button_packet import ButtonPacket
 
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
 
+from definitions import BAUD_RATE
+
 
 def main():
     ble = BLERadio()
     uart_service = UARTService()
     advertisement = ProvideServicesAdvertisement(uart_service)
-
-    pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.1)
+    uart = busio.UART(board.TX, board.RX, baudrate=BAUD_RATE, timeout=0)
 
     while True:
         # Advertise when not connected.
@@ -32,11 +32,52 @@ def main():
         while ble.connected:
             if uart_service.in_waiting:
                 packet = Packet.from_stream(uart_service)
-                print(packet)
+
                 if isinstance(packet, ButtonPacket):
-                    print(packet.button)
-                    string = "Helloooo"
+                    btn = int(packet.button)
+                    print(btn)
+
+                    if up_arrow_pressed(btn):
+                        m1 = -1
+                        m2 = -1
+                    elif down_arrow_pressed(btn):
+                        m1 = 1
+                        m2 = 1
+                    elif left_arrow_pressed(btn):
+                        m1 = -1
+                        m2 = 1
+                    elif right_arrow_pressed(btn):
+                        m1 = 1
+                        m2 = -1
+                    elif stop_pressed(btn):
+                        m1 = 0
+                        m2 = 0
+                    else:
+                        continue
+
+                    string = f"{m1}, {m2}"
                     uart.write(bytes(f"<B,{string}>", "ascii"))
+                    print(m1, m2)
+
+
+def up_arrow_pressed(btn):
+    return btn == 5
+
+
+def down_arrow_pressed(btn):
+    return btn == 6
+
+
+def left_arrow_pressed(btn):
+    return btn == 7
+
+
+def right_arrow_pressed(btn):
+    return btn == 8
+
+
+def stop_pressed(btn):
+    return btn == 1
 
 
 if __name__ == "__main__":
